4416ee7645bfe4e3fc8da71040f75da30809595a
Thais-ra <rotinasoftware@gmail.com>
2021-03-02 18:36:13 -0300

    Commit inicial

---
+++ calculator/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:calculator/views/calculator.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'calculator.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+    ]);
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Calculator(),
+    );
+  }
+}
